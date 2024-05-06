408f0de3012284d93203bba992d66b385a22509b
Mohammad Ashif Mondal <91359633+Ashif6290@users.noreply.github.com>
2024-05-06 20:37:35 +0530

    all done expect button toggle

--- Project/app/src/App.jsx
+++ Project/app/src/App.jsx
@@ -4,8 +4,10 @@ import SearchResults from "./components/SearchResults/SearchResults";
 export const BASE_URL = "http://localhost:9000";
 const App = () => {
   const [ data , setData ] = useState(null);
+  const [filteredData , setFilteredData] = useState(null);
   const [ loading , setLoading ] = useState(false);
   const [error , setError] = useState(null);
+  const [selectedBtn , setSelectedBtn] = useState("all");
   
   useEffect(() => {
     const fetchFoodData = async () => {
@@ -14,6 +16,7 @@ const App = () => {
         const response = await fetch(BASE_URL);
         const json = await response.json();
         setData(json);
+        setFilteredData(json);
         setLoading(false);
       } catch (error) {
         setError("Unable to fetch data");
@@ -22,38 +25,68 @@ const App = () => {
     fetchFoodData();
   }, []);
   
+  const searchFood = (e) => {
+    const searchValue = e.target.value;
+    console.log(searchValue);
+
+    if (searchValue === "") {
+      setFilteredData(null);
+    }
+
+    const filter = data?.filter((food) =>
+      food.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    setFilteredData(filter);
+  }
+  const filterFood = (type) => {
+    if(type==="all"){
+      setFilteredData(data);
+      setSelectedBtn("all");
+      return;
+    }
+    const filter = data?.filter((food) =>
+      food.type.toLowerCase().includes(type.toLowerCase())
+    );
+    setFilteredData(filter)
+    setSelectedBtn(type)  
+  }
   
   if(error) return <div>{error}</div>;
   if(loading) return <div>loading.....</div>;
 
 
-  return <Container>
-    <TopContainer>
-      <div className="logo">
-        <img src="/logo.svg" alt="logo" />
-      </div>
-      <div className="search">
-        <input placeholder="Search Food" />
-      </div>
-    </TopContainer>
-    <FilterContainer>
-      <Button>All</Button>
-      <Button>Breakfast</Button>
-      <Button>Lunch</Button>
-      <Button>Dinner</Button>
-    </FilterContainer>
-    <SearchResults data={data}/>
-  </Container>
+  return (
+    <>
+      <Container>
+        <TopContainer>
+          <div className="logo">
+            <img src="/logo.svg" alt="logo" />
+          </div>
+          <div className="search">
+            <input onChange={searchFood} placeholder="Search Food" />
+          </div>
+        </TopContainer>
+        <FilterContainer>
+          <Button onClick={() => filterFood("all")}>All</Button>
+          <Button onClick={() => filterFood("breakfast")}>Breakfast</Button>
+          <Button onClick={() => filterFood("lunch")}>Lunch</Button>
+          <Button onClick={() => filterFood("dinner")}>Dinner</Button>
+        </FilterContainer>
+
+      </Container>
+      <SearchResults data={filteredData} />
+    </>
+  )
 }
 
 export default App;
 
-const Container = styled.div`
+export const Container = styled.div`
   max-width: 1200px;
   margin: 0 auto;
 `;
 const TopContainer = styled.div`
-  min-height: 140px;
+  height: 140px;
   display: flex;
   justify-content: space-between;
   padding: 16px;
@@ -69,6 +102,10 @@ const TopContainer = styled.div`
       padding: 0 10px;
     }
   }
+  @media (0 < width < 600px) {
+    flex-direction: column;
+    height: 120px;
+  }
 `;
 const FilterContainer = styled.section`
   display: flex;
@@ -76,10 +113,16 @@ const FilterContainer = styled.section`
   gap: 12px;
   padding-bottom: 40px;
 `;
-const Button = styled.button`
+export const Button = styled.button`
   background: #ff4343;
   border-radius: 5px;
   padding: 6px 12px;
   border: none;
   color: white;
+  cursor: pointer;
+  margin-bottom: 8px;
+  margin-right: 8px;
+  &:hover {
+    background-color: #ff1414
+  }
 `;
